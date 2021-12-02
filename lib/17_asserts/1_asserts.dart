c5caa8b55de0d2da177b6860c77800cf44a5298a
leo <realleo2009@hotmail.com>
2021-12-02 17:56:00 -0300

    dart poo

---
+++ lib/17_asserts/1_asserts.dart
@@ -0,0 +1,6 @@
+import 'package:dart_poo/17_asserts/cliente.dart';
+
+void main() {
+
+  var cli = Cliente();
+}
---
+++ lib/17_asserts/cliente.dart
@@ -0,0 +1,15 @@
+class Cliente {
+  String? nome;
+  String? cpf;
+
+  String? razaoSocial;
+  String? cnpj;
+
+  Cliente({this.nome, this.cpf, this.razaoSocial, this.cnpj})
+      : assert(
+          (cpf != null)
+              ? nome != null && razaoSocial == null && cnpj == null
+              : true,
+          'Voce enviou cpf junto com cnpj',
+        );
+}
